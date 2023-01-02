91f73f8c7e4ee2a971a26efc992f5e96d2fff1ea
Anouk <anouk.wijnen2@gmail.com>
2023-01-02 12:07:20 +0100

    save before test

--- prototype-escaperoom/src/Game.js
+++ prototype-escaperoom/src/Game.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser'
 
-export default class Game extends Phaser.Scene {
+export default class Game extends Phaser.Scene 
+{
+	
 	constructor() {
 		super('hello-world')
 	}
@@ -11,10 +13,13 @@ export default class Game extends Phaser.Scene {
 			frameWidth: 64,
 			startFrame:0
 		})
+
+		this.cursors = this.input.keyboard.createCursorKeys()
 	}
 
 	create() 
 	{
+		//level layout
 		const level = [
 			[100, 	 100, 	100, 	100, 	100, 	100, 	  100, 	  100,		100,	100,],
 			[100, 	   0, 	  0, 	  0, 	  0, 	  0, 		0, 		0,		0,		100,],
@@ -25,7 +30,7 @@ export default class Game extends Phaser.Scene {
 			[100,	   0, 	  0, 	  0,	  0, 	  0, 		0, 		0,		0,		100,],
 			[100,	 100,	100, 	100,	100, 	100, 	  100, 	  100,		100,	100,]
 		]
-
+		//size of the area
 		const map = this.make.tilemap({ 
 			data: level, 
 			tileWidth:64, 
@@ -34,5 +39,20 @@ export default class Game extends Phaser.Scene {
 
 		const tiles = map.addTilesetImage('tiles')
 		const layer = map.createStaticLayer(0, tiles, 0, 0)
+
+		this.add.sprite(400, 300, 'tiles', 52)
+
+		//player walking animations
+		this.anims.create({
+			key: 'left',
+			frames: this.anims.generateFrameNumbers('tiles', { start: 81, end: 83 }),
+			frameRate: 10,
+			repeat: -1
+		})
+	}
+
+	update()
+	{
+		
 	}
 }
