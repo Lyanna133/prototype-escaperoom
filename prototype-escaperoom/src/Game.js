e20dcf9bb023e9d77290d09561aa37307d2e2175
Anouk <anouk.wijnen2@gmail.com>
2023-01-02 11:22:15 +0100

    added spritesheet and removed helloworld

---
+++ prototype-escaperoom/src/Game.js
@@ -0,0 +1,15 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene {
+	constructor() {
+		super('hello-world')
+	}
+
+	preload() {
+		
+	}
+
+	create() {
+		
+	}
+}
--- prototype-escaperoom/src/main.js
+++ prototype-escaperoom/src/main.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './HelloWorldScene'
+import Game from './Game'
 
 const config = {
 	type: Phaser.AUTO,
@@ -13,7 +13,7 @@ const config = {
 			gravity: { y: 200 },
 		},
 	},
-	scene: [HelloWorldScene],
+	scene: [Game],
 }
 
 export default new Phaser.Game(config)
