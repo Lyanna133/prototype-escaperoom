5f653b14694e63e0116ac958a130cf554f1d0979
Anouk <anouk.wijnen2@gmail.com>
2023-01-02 14:05:41 +0100

    walking +idle animations fixed

--- prototype-escaperoom/src/Game.ts
+++ prototype-escaperoom/src/Game.ts
@@ -44,11 +44,48 @@ export default class Game extends Phaser.Scene
 		const layer = map.createStaticLayer(0, tiles, 0, 0)
 
 		this.player = this.add.sprite(400, 300, 'tiles', 52)
+		
+		//player walking and idle animations
+		this.anims.create({
+			key: 'idle-down',
+			frames: [ {key: 'tiles', frame: 52} ]
+		})
+		this.anims.create({
+			key: 'idle-left',
+			frames: [ {key: 'tiles', frame: 81} ]
+		})
+		this.anims.create({
+			key: 'idle-right',
+			frames: [ {key: 'tiles', frame: 78} ]
+		})
+		this.anims.create({
+			key: 'idle-up',
+			frames: [ {key: 'tiles', frame: 55} ]
+		})
 
-		//player walking animations
 		this.anims.create({
 			key: 'left',
 			frames: this.anims.generateFrameNumbers('tiles', { start: 81, end: 83 }),
+			frameRate: 10,
+			repeat: -1
+		})
+
+		this.anims.create({
+			key: 'right',
+			frames: this.anims.generateFrameNumbers('tiles', { start: 78, end: 80 }),
+			frameRate: 10,
+			repeat: -1
+		})
+
+		this.anims.create({
+			key: 'up',
+			frames: this.anims.generateFrameNumbers('tiles', { start: 55, end: 57 }),
+			frameRate: 10,
+			repeat: -1
+		})
+		this.anims.create({
+			key: 'down',
+			frames: this.anims.generateFrameNumbers('tiles', { start: 52, end: 54 }),
 			frameRate: 10,
 			repeat: -1
 		})
@@ -56,13 +93,35 @@ export default class Game extends Phaser.Scene
 
 	update()
 	{
+		//the walking animations
 		if (!this.cursors || !this.player)
 		{
 			return
 		}
 		if (this.cursors.left.isDown)
 		{
-			this.player.anims.play('left')
+			this.player.anims.play('left', true)
+		}
+		else if (this.cursors.right.isDown)
+		{
+			this.player.anims.play('right', true)
+		}
+		else if (this.cursors.up.isDown)
+		{
+			this.player.anims.play('up', true)
+		}
+		else if (this.cursors.down.isDown)
+		{
+			this.player.anims.play('down', true)
+		}
+		else if (this.player.anims.currentAnim)
+		{
+			const key = this.player?.anims.currentAnim?.key
+			if (!key.startsWith(`idle-`))
+			{
+				this.player.anims.play(`idle-${key}`, true)
+			}
+			
 		}
 	}
 }
